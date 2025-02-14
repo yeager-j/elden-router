da6fd8b9f9e00955a1191ca95fe2012888ec1b81
Jackson Yeager <jacksonyeager@gmail.com>
2025-02-14 18:19:07 -0500

    Fixed title and meta tags

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,7 +3,7 @@ import { EB_Garamond, Geist, Geist_Mono } from "next/font/google";
 import "@workspace/ui/globals.css";
 
 import { Providers } from "@/components/providers";
-import Head from "next/head";
+import { Metadata } from "next";
 
 import { Toaster } from "@workspace/ui/components/sonner";
 
@@ -22,6 +22,28 @@ const fontMono = Geist_Mono({
   variable: "--font-mono",
 });
 
+export const metadata: Metadata = {
+  title: "Guidance of Grace – Elden Ring Route Planner",
+  description:
+    "Let the Guidance of Grace lead you. A powerful tool to chart your path through the Lands Between and find the best route to any item you seek.",
+  keywords: [
+    "Elden Ring",
+    "Elden Ring map",
+    "item finder",
+    "best route",
+    "Guidance of Grace",
+    "Elden Ring guide",
+    "Elden Ring tools",
+  ],
+  openGraph: {
+    type: "website",
+    url: "https://guidanceofgrace.com",
+    title: "Guidance of Grace – Elden Ring Route Planner",
+    description:
+      "Let the Guidance of Grace lead you. A powerful tool to chart your path through the Lands Between and find the best route to any item you seek.",
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -29,29 +51,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Head>
-        <title>Guidance of Grace</title>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta
-          name="keywords"
-          content="Elden Ring, Elden Ring map, item finder, best route, Guidance of Grace, Elden Ring guide, Elden Ring tools"
-        />
-
-        <meta property="og:type" content="website" key="type" />
-        <meta property="og:title" content="Guidance of Grace" key="title" />
-        <meta
-          property="og:description"
-          content="Let the Guidance of Grace lead you. A powerful tool to chart your path through the Lands Between and find the best route to any item you seek."
-          key="description"
-        />
-        <meta
-          property="og:url"
-          content="https://guidanceofgrace.com"
-          key="url"
-        />
-      </Head>
-
       <body
         className={`${fontSerif.variable} ${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
